60c17cd2902d60bd23fe47cf8d0832d52359a99e
Andrew Park <ajpark5@uci.edu>
2024-04-24 11:28:16 -0700

    Android Release Tweaks

--- soberguardianflutter/lib/shared/icons.dart
+++ soberguardianflutter/lib/shared/icons.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Pair<String, IconData> {
-    String first;
-    IconData last;
+  String first;
+  IconData last;
 
-    Pair(this.first, this.last);
+  Pair(this.first, this.last);
 }
 
 List<Pair<String, IconData>> supportedIcons = [
-    Pair("Snowflake", const IconData(0xe037, fontFamily: 'MaterialIcons')), // ac_unit constant
-    Pair("Standing Person", const IconData(0xe03c, fontFamily: 'MaterialIcons')), // accessibility constant
-    Pair("Government", const IconData(0xe040, fontFamily: 'MaterialIcons')), // account_balance constant
+  Pair("Snowflake",
+      const IconData(0xe037, fontFamily: 'MaterialIcons')), // ac_unit constant
+  Pair(
+      "Standing Person",
+      const IconData(0xe03c,
+          fontFamily: 'MaterialIcons')), // accessibility constant
+  Pair(
+      "Government",
+      const IconData(0xe040,
+          fontFamily: 'MaterialIcons')), // account_balance constant
 ];
 
 Map<IconData, String> iconsToString = {
-    IconData(0xe037, fontFamily: 'MaterialIcons'): "e037",
-    IconData(0xe03c, fontFamily: 'MaterialIcons'): "e03c",
-    IconData(0xe040, fontFamily: 'MaterialIcons'): "e040",
+  const IconData(0xe037, fontFamily: 'MaterialIcons'): "e037",
+  const IconData(0xe03c, fontFamily: 'MaterialIcons'): "e03c",
+  const IconData(0xe040, fontFamily: 'MaterialIcons'): "e040",
 };
 
 // Map<String, IconData> supportedIcons1 = {
